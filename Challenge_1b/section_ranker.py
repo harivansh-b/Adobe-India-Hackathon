d1ae3384484928701e2fb5e0bb53ee51ec20d84b
KeshavKS2005 <168101874+KeshavKS2005@users.noreply.github.com>
2025-07-27 19:01:26 +0530

    Update section_ranker.py

--- Challenge_1b/section_ranker.py
+++ Challenge_1b/section_ranker.py
@@ -1,14 +1,20 @@
 from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
-def rank_sections(sections,persona_job_embedding,model):
+
+
+def rank_sections(sections, persona_job_embedding, model, similarity_threshold=0.40):
+    ranked_sections = []
+
     for section in sections:
-        print(section)
         section_emb = model.encode([section["text"]])[0]
         similarity = cosine_similarity(
             [persona_job_embedding],
             [section_emb]
         )[0][0]
         section["score"] = float(similarity)
-    ranked = sorted(sections,key=lambda section: section["score"],reverse=True)
-    print(ranked)
+
+        if similarity >= similarity_threshold:
+            ranked_sections.append(section)  # Keep only if above threshold
+
+    ranked = sorted(ranked_sections, key=lambda section: section["score"], reverse=True)
+
     return ranked
